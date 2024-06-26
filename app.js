5d9984cee514acb1bbe073e9d157962e1724d2db
Ayush891f <127117290+Ayush891f@users.noreply.github.com>
2024-06-26 08:07:49 +0530

    files added

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const otpRouter = require('./Routes/otpRoutes');
-
+const path=require("path");
+const build = require("build");
 app.use(express.json());
 app.use('/api', otpRouter);
 
 
 const PORT = process.env.PORT || 5000;
+app.get("/",(req,res)=>{
+  app.use(express.static(path.resolve(__dirname,"frontend","build")));
+  res.sendFile(path.resolve(__dirname,"frontend",build,"index.html"));
+});
+app.use(express.static(build));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
